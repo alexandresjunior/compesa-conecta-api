ccc3ff27f139e2a1d25513e632960b873cf4a7b3
Alexandre Junior <souzajr.alexandre@gmail.com>
2025-09-28 11:07:11 -0300

    Criação do projeto spring boot

---
+++ src/main/java/br/com/compesa/conecta/api/CompesaConectaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.compesa.conecta.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompesaConectaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompesaConectaApiApplication.class, args);
+	}
+
+}
